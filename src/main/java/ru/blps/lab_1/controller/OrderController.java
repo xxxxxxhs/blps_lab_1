4532e4fb35fe898b151e836f1f4b62dfee2db95f
shamatulskiy.r <fkqly@yandex.ru>
2026-02-28 11:58:52 +0300

    exception handler

--- src/main/java/ru/blps/lab_1/controller/OrderController.java
+++ src/main/java/ru/blps/lab_1/controller/OrderController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.blps.lab_1.dto.OrderDto;
-import ru.blps.lab_1.entity.Order;
 import ru.blps.lab_1.service.OrderService;
 
 @RestController
@@ -20,37 +19,37 @@ public class OrderController {
     }
 
     @PostMapping
-    public Order createOrder() {
+    public OrderDto createOrder() {
         return orderService.createOrder();
     }
 
     @PostMapping("/{orderId}/assign")
-    public Order assignOrder(@PathVariable Long orderId) {
+    public OrderDto assignOrder(@PathVariable Long orderId) {
         return orderService.assignOrder(orderId);
     }
 
     @PostMapping("/{orderId}/accept")
-    public Order acceptOrder(@PathVariable Long orderId) {
+    public OrderDto acceptOrder(@PathVariable Long orderId) {
         return orderService.acceptOrder(orderId);
     }
 
     @PostMapping("/{orderId}/reject")
-    public Order rejectOrder(@PathVariable Long orderId) {
+    public OrderDto rejectOrder(@PathVariable Long orderId) {
         return orderService.rejectOrder(orderId);
     }
 
     @PostMapping("/{orderId}/pickup")
-    public Order pickupOrder(@PathVariable Long orderId) {
+    public OrderDto pickupOrder(@PathVariable Long orderId) {
         return orderService.pickupOrder(orderId);
     }
 
     @PostMapping("/{orderId}/complete")
-    public Order completeOrder(@PathVariable Long orderId) {
+    public OrderDto completeOrder(@PathVariable Long orderId) {
         return orderService.completeOrder(orderId);
     }
 
     @PostMapping("/{orderId}/cancel")
-    public Order cancelOrder(@PathVariable Long orderId) {
+    public OrderDto cancelOrder(@PathVariable Long orderId) {
         return orderService.cancelOrder(orderId);
     }
 
---
+++ src/main/java/ru/blps/lab_1/controller/RestExceptionHandler.java
@@ -0,0 +1,23 @@
+package ru.blps.lab_1.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFound(NoSuchElementException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalState(IllegalStateException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
+
