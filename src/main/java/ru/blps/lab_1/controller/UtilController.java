c30c4a6c4334745df41a4a98ae84baae09488241
fkqly <fqy@MacBook-Pro-27.local>
2026-07-30 22:36:46 +0300

    logging

--- src/main/java/ru/blps/lab_1/controller/UtilController.java
+++ src/main/java/ru/blps/lab_1/controller/UtilController.java
@@ -1,5 +1,7 @@
 package ru.blps.lab_1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/util")
 public class UtilController {
 
+    public static final Logger logger = LoggerFactory.getLogger(UtilController.class);
+
     @Value("${spring.application.instance-num}")
     private String instanceNum;
     
+    private Integer attempt = 1;
+
     @GetMapping("/info")
     public ResponseEntity<String> info() {
+        logger.atInfo().addKeyValue("attempt", this.attempt++).log("==SIMPLE API CALL==");
         return ResponseEntity.ok("Instancenumber: " + instanceNum);
     }
 }
